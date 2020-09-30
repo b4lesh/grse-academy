473b51ae65cb6ef3794669497a515dbac6a86d3d
George <peretolchin.george@gmail.com>
2020-09-30 16:35:14 +0300

    Сделана база, но пока кривое удаление

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,43 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+// interface ITodoList {
+//   id: number;
+//   text: string;
+//   isDone: boolean;
+// }
 export class AppComponent {
-  title = 'grse-academy';
+  todoList = [{ id: 1, text: 'Сходить в магазин', isDone: false }];
+  inputId: number;
+  inputChangedText: string;
+  inputNewText: string;
+  inputIsDone: boolean;
+
+  changeTask(): void {
+    const inputId: number = Number(this.inputId);
+    const inputText = this.inputChangedText;
+    const inputIsDone = this.inputIsDone;
+    let isBe = false;
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < this.todoList.length; i++) {
+      if (this.todoList[i].id === inputId) {
+        this.todoList.splice(i, 1);
+        console.log(this.todoList);
+        isBe = true;
+        break;
+      }
+    }
+    if (isBe) {
+      this.todoList.push({ id: inputId, text: inputText, isDone: inputIsDone });
+    }
+  }
+
+  addTask(): void {
+    const inputId = this.todoList.length + 1;
+    const inputText = this.inputNewText;
+    const inputIsDone = false;
+    if (inputText) {
+      this.todoList.push({ id: inputId, text: inputText, isDone: inputIsDone });
+    }
+  }
 }
