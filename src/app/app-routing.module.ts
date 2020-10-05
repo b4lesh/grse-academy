fe3478d1e738564837a170bad6148e4d10435bf4
George <peretolchin.george@gmail.com>
2020-10-05 13:25:04 +0300

    Настройка роутинга основного компонента. Добавлен редирект на пустую страницу

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
 
 const routes: Routes = [
-  { path: '', component: NavigationBarComponent },
+  { path: '', redirectTo: '', pathMatch: 'full' },
   {
     path: 'todo-list',
     loadChildren: () =>
