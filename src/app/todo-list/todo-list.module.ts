2a76e62c84f3e7d53351aa05d40338e9fd4d492c
George <peretolchin.george@gmail.com>
2020-10-14 01:36:57 +0300

    Создание "ленивой" загрузки

---
+++ src/app/todo-list/todo-list.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { TodoListRoutingModule } from './todo-list-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, TodoListRoutingModule],
+})
+export class TodoListModule {}
