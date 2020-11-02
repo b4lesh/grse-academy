f9c7088f4121679f86a7ae31a86e82832feebede
George <peretolchin.george@gmail.com>
2020-11-02 17:31:26 +0300

    Добавлена страница для входа

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '', pathMatch: 'full' },
@@ -10,6 +11,7 @@ const routes: Routes = [
         (module) => module.TodoListModule
       ),
   },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,15 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TodoListComponent } from './todo-list/todo-list.component';
 import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [AppComponent, TodoListComponent, NavigationBarComponent],
+  declarations: [
+    AppComponent,
+    TodoListComponent,
+    NavigationBarComponent,
+    LoginComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule, FormsModule, ReactiveFormsModule],
   providers: [],
   bootstrap: [AppComponent],
