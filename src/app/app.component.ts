6a83860a13461c1fe2026d04e36f37b84bc4a45c
George <peretolchin.george@gmail.com>
2020-11-18 12:37:52 +0300

    Обновление до Angular 11.0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-// import { User } from './modules/user';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  constructor() {
-    // const users: Array<User> = [
-    //   { id: 1, username: 'George', password: 'Qwerty12' },
-    // ];
-    // localStorage.setItem('user', JSON.stringify(users));
-  }
-
-  ngOnInit(): void {}
-
-  ngOnDestroy(): void {}
-}
+export class AppComponent {}
