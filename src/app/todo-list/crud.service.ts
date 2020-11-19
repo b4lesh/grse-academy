108e88b20b90dd0fb6e7b34f88cc727ae2f0d62e
George <peretolchin.george@gmail.com>
2020-11-19 13:32:17 +0300

    Добавлен метод на получение всех данных

--- src/app/todo-list/crud.service.ts
+++ src/app/todo-list/crud.service.ts
@@ -1,11 +1,16 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CrudService {
   constructor(private angularFirestore: AngularFirestore) {}
+
+  getAllTasks(): Observable<any> {
+    return this.angularFirestore.collection('todo-list').snapshotChanges();
+  }
 
   addTask(task): any {
     return this.angularFirestore.collection('todo-list').add(task);
